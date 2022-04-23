df925127afcc07b2438eae20833b278f21de8ded
Rakmo10497 <odeshpan@usc.edu>
2022-04-23 05:54:26 +0530

    Flutter basic application setup

---
+++ photo_gallery_editor/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PhotoEditorLauncher());
+}
+
+class PhotoEditorLauncher extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text('Hello!'));
+  }
+}
